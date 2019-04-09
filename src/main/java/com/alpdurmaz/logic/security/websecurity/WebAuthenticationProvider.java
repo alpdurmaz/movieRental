e58f4b1029a64c55b46f73f4b420c13c7aeed105
alpdurmaz <alp_durmaz@hotmail.com>
2019-04-09 19:14:57 +0800

    deploy.sh added

--- src/main/java/com/alpdurmaz/logic/security/websecurity/WebAuthenticationProvider.java
+++ src/main/java/com/alpdurmaz/logic/security/websecurity/WebAuthenticationProvider.java
@@ -7,6 +7,8 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
